11cc3eac435e1c8c8a964b2140cfd642424cdfcc
Kieran Kunhya <kierank@obe.tv>
2020-05-21 18:13:43 +0100

    Disable crashing AVX2

--- common/quant.c
+++ common/quant.c
@@ -719,7 +719,7 @@ void x264_quant_init( x264_t *h, int cpu, x264_quant_function_t *pf )
     {
         pf->quant_4x4 = x264_quant_4x4_avx2;
         pf->quant_4x4_dc = x264_quant_4x4_dc_avx2;
-        pf->quant_8x8 = x264_quant_8x8_avx2;
+        //pf->quant_8x8 = x264_quant_8x8_avx2;
         pf->quant_4x4x4 = x264_quant_4x4x4_avx2;
         if( cpu&X264_CPU_LZCNT )
             pf->coeff_last[DCT_LUMA_8x8] = x264_coeff_last64_avx2_lzcnt;
